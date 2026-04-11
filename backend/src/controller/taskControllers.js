50833107935bdb6c83fa54d0d90e90ac2aa05ae5
mikeyvu <minhvu2614@gmail.com>
2026-04-11 11:19:15 +1000

    feature(CRUD): Implement CRUD operation and connect frontend with backend api, displaying data to the client interfact
    -
    -

--- backend/src/controller/taskControllers.js
+++ backend/src/controller/taskControllers.js
@@ -2,30 +2,44 @@ import Task from "../models/Task.js";
 
 export const getAllTasks = async (req, res) => {
     try {
-        const tasks = await Task.find().sort({createdAt: 'desc'});
-        res.status(200).json(tasks);
+        const result = await Task.aggregate([
+            {
+                $facet: {
+                    tasks: [{ $sort: { createdAt: -1 } }],
+                    activeCount: [{ $match: { status: "active" } }, { $count: "count" }],
+                    completeCount: [{ $match: { status: "completed" } }, { $count: "count" }],
+                }
+            }
+
+        ])
+
+        const tasks = result[0].tasks;
+        const activeCount = result[0].activeCount[0]?.count || 0;
+        const completeCount = result[0].completeCount[0]?.count || 0;
+
+        res.status(200).json({tasks, activeCount, completeCount});
     } catch (error) {
         console.error("Error while calling getAllTasks", error);
-        res.status(500).json({message: "Interval error"})
+        res.status(500).json({ message: "Interval error" })
     }
 };
 
 export const createTask = async (req, res) => {
     try {
-        const {title} = req.body;
-        const task = new Task({title});
+        const { title } = req.body;
+        const task = new Task({ title });
 
         const newTask = await task.save();
         res.status(201).json(newTask);
     } catch (error) {
         console.error("Error while calling createTask", error);
-        res.status(500).json({message: "Interval error"})
+        res.status(500).json({ message: "Interval error" })
     }
 };
 
 export const updateTask = async (req, res) => {
     try {
-        const {title, status, completedAt} = req.body;
+        const { title, status, completedAt } = req.body;
 
         // Save the action into a variable to check if that action was successful or not
         const updatedTask = await Task.findByIdAndUpdate(
@@ -39,14 +53,14 @@ export const updateTask = async (req, res) => {
         )
 
         if (!updatedTask) {
-            return res.status(404).json({message: "task not found"});
+            return res.status(404).json({ message: "task not found" });
         }
 
         return res.status(200).json(updatedTask);
     } catch (error) {
         console.error("Error while calling updateTask", error);
         if (res.headersSent) return;
-        return res.status(500).json({message: "Interval error"});
+        return res.status(500).json({ message: "Interval error" });
     }
 };
 
@@ -55,13 +69,13 @@ export const deleteTask = async (req, res) => {
         const deletedTask = await Task.findByIdAndDelete(req.params.id);
 
         if (!deletedTask) {
-            return res.status(404).json({message: "task not found"});
+            return res.status(404).json({ message: "task not found" });
         }
 
         return res.status(200).json(deletedTask);
     } catch (error) {
         console.error("Error while calling deleteTask", error);
         if (res.headersSent) return;
-        return res.status(500).json({message: "Interval error"});
+        return res.status(500).json({ message: "Interval error" });
     }
 }
